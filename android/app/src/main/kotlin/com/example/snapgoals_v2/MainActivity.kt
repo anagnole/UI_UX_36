9e4d269f34aab533790d1b40b635e778affecacc
gikontos <giorgoskontos911@gmail.com>
2024-01-08 16:28:40 +0200

    initial commit

---
+++ android/app/src/main/kotlin/com/example/snapgoals_v2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.snapgoals_v2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/home/home.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget{
+  HomePage();
+
+ @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Card(
+          shadowColor: Colors.transparent,
+          margin: const EdgeInsets.all(8.0),
+          child: SizedBox.expand(
+            child: Center(
+              child: Text(
+                'Home page',
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+            ),
+          ),
+        ),
+    );
+  }
+}
