28e659fbfd00c44d9b9fbb68bcae3d3177b0f313
gikontos <giorgoskontos911@gmail.com>
2024-01-09 03:55:57 +0200

    top bar for camera and profile page

---
+++ lib/src/appbar_withx.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:snapgoals_v2/src/home/home.dart';
+import 'package:snapgoals_v2/src/profile/profile.dart';
+
+
+const primaryColor= Color(0xFF33228E);
+
+class SnapGoalsAppBar2 extends StatelessWidget implements PreferredSizeWidget {
+  
+
+  const SnapGoalsAppBar2();
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+  
+  // Calculate spacing as a percentage of screen width
+    final titleSpacingPercentage = 0.1; 
+    final iconPaddingPercentage = 0.05; 
+
+
+    return AppBar(
+      backgroundColor: primaryColor, // Replace with your specific color
+      title: Text(
+        "SnapGoals",
+        style: TextStyle(
+          color: Colors.white, 
+          fontFamily: 'Lobster',// Text color
+          shadows: [
+            Shadow(
+                color: Colors.black, // Shadow color
+               offset: Offset(0, 4), // Offset of the shadow (horizontal, vertical)
+               blurRadius: 4, // Blur radius
+      ),
+    ],
+        ),
+      ),
+      titleSpacing: screenWidth * titleSpacingPercentage,
+      
+      actions: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: screenWidth * iconPaddingPercentage),
+          child: IconButton(
+            onPressed: (){
+              Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => HomePage()),
+            );}, 
+            icon: Image.asset("assets/images/close_24px.png"),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+}
--- lib/src/profile/profile.dart
+++ lib/src/profile/profile.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
+import '../appbar_withx.dart';
 
 class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Profile Page'),
-      ),
-      body: Center(
-        child: Text(
-          'This is the profile page',
-          style: TextStyle(fontSize: 20),
+      appBar: SnapGoalsAppBar2(),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/background.png'), // Replace with your image path
+            fit: BoxFit.fill, // Adjust the image fit as needed
+          ),
+
         ),
       ),
     );
